e10debe7a3da603422ff6391a96a97cda54ec2ae
marisa <ma-ah02@live.com>
2019-01-24 00:17:41 -0200

    avanço no js

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,16 @@
-function encode(){
+function encode(offset, message){
     const messageNoCodif = document.getElementById("message").value;
     const offsetMsg = document.getElementById("offset").value;
+    const result = [];
 
-    
-}
+    for(let i = 0; i < messageNoCodif.length; i++){
+        result[i] = messageNoCodif.charCodeAt(i);
+        let b = result[i]
+        const a = ((result - 32 + offset)%26)+ 32;
+        
+        
+        }
+    return result;
+
+
+    }
