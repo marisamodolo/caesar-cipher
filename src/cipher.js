b4c47cae1c1b514dec941e63ff043111fb329672
Laboratória <ma-ah02@live.com>
2019-01-24 17:01:33 -0200

    atualizaçao do js

--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,57 @@
 function encode(offset, message){
-    const messageNoCodif = document.getElementById("message").value;
-    const offsetMsg = document.getElementById("offset").value;
-    const result = [];
+    let messageNoCodif = document.getElementById("message").value;
+    let offsetMsg = document.getElementById("offset").value;
+
+    let result = []; 
+    let x = parseInt(offsetMsg); 
+    let b = [];
+    let y; 
+    let a;
 
     for(let i = 0; i < messageNoCodif.length; i++){
         result[i] = messageNoCodif.charCodeAt(i);
-        let b = result[i]
-        const a = ((result - 32 + offset)%26)+ 32;
+       // if(x > 0){
+            a = (( result[i] - 32 + x)%94)+ 32;
+       // }
+        /*if(x < 0){
+            a = (( result[i] - 65 - (-x))%26)+ 65;
+         console.log("negativo")
+        }*/
+        b[i] = String.fromCharCode(a);
+        y = b.join("");
+        console.log(b); 
+        }  
+        document.getElementById("resultmsg").innerHTML = y;
+    }
+//criar função para encode e decode que use os paramentros;
+    function decode(offset, messageDecode){
+    let messageNoCodif = document.getElementById("messageDecode").value;
+    let offsetMsg = document.getElementById("offset").value;
+
+    let result = []; 
+    let x = parseInt(offsetMsg); 
+    let b = [];
+    let y; 
+    let a;
+
+    for(let i = 0; i < messageNoCodif.length; i++){
+        result[i] = messageNoCodif.charCodeAt(i);
+        //if(x > 0){
+            a = ((( result[i] - 32 + -x)%94)+32);
+            
+        //}
+        /*if(x < 0){
+            a = (( result[i] - 65 - (-x))%26)+ 65;
+         console.log("negativo")
+        }*/
+        b[i] = String.fromCharCode(a);
+        y = b.join("");
         
-        
-        }
-    return result;
+        console.log(a); 
+        }  
+        document.getElementById("resultmsg").innerHTML = y;
 
 
     }
+
+    function caesarEncode()
