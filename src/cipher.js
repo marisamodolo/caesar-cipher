4d3195bacfe13b750a9a1b8777deb8fefb28513f
marisa <ma-ah02@live.com>
2019-01-25 20:56:09 -0200

    atualização do css

--- src/cipher.js
+++ src/cipher.js
@@ -1,19 +1,36 @@
 let result;
 let finalAscii;
-let msgResult;
+let msgResultEncode;
+let msgResultDecode;
+let mudarDiv;
 
 function caesarEncode(){
-    const string = document.getElementById("message").value;
-    const offset = document.getElementById("offset").value;
-    msgResult = encode(offset, string);
-    document.getElementById("resultmsg").innerHTML = msgResult;
+  console.log("entrou no caesar encode");
+    let string = document.getElementById("message").value;
+    let offset = document.getElementById("offset").value;
+    msgResultEncode = encode(offset, string);
+    document.getElementById("resultMsg1").innerHTML = msgResultEncode;
+    mudarDiv = document.getElementById('boxMsgEncode').style.display;
+  
+    if(mudarDiv == "none"){
+      document.getElementById('boxMsgEncode').style.display = 'block';
+      document.getElementById('boxMsgDecode').style.display = 'none';
+    }
+    
 }
 
 function caesarDecode(){
-    const string = document.getElementById("messageDecode").value;
-    const offset = document.getElementById("offset").value;
-    msgResult = decode(offset, string);
-    document.getElementById("resultmsg").innerHTML = msgResult;
+  console.log("entrou no caesar decode");
+    let string = document.getElementById("messageDecode").value;
+    let offset = document.getElementById("offset2").value;
+    msgResultDecode = decode(offset, string);
+    document.getElementById("resultMsg2").innerHTML = msgResultDecode;
+    mudarDiv = document.getElementById('boxMsgDecode').style.display;
+  
+    if(mudarDiv == "none"){
+      document.getElementById('boxMsgDecode').style.display = 'block';
+      document.getElementById('boxMsgEncode').style.display = 'none';
+    }
 }
 
 function encode(offset, string){
@@ -36,7 +53,11 @@ function decode(offset, string){
 
     for(let i=0; i < string.length; i++){
       currentLetter[i] = string.charCodeAt(i);
-      finalAscii = ((currentLetter[i] - 33 - offset)%94)+ 33;
+      if( offset >=62){
+       
+       console.log("entrou no maior que 62")
+      }
+      finalAscii = ((currentLetter[i] - 33 - offset)%94)+33;
       currentLetter[i] = String.fromCharCode(finalAscii);
       result = currentLetter.join("");
       console.log(finalAscii);
